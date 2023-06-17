40fa3c90fe56a11d7ca15d1310bea8809ca475c1
Rohan Joshi <rohanjoshi81@gmail.com>
2023-06-17 10:08:16 +0530

    Fixed 2 pages
    Some fixes

--- assets/js/experience.js
+++ assets/js/experience.js
@@ -5,26 +5,45 @@ AOS.init();
 const experiencecards = document.querySelector(".experience-cards");
 const exp = [
   {
-    title: "Software Development Intern",
-    cardImage: "assets/images/experience-page/flipkart.jpg",
-    place: "Flipkart",
-    time: "(May, 2020 - present)",
-    desp: "<li>Worked to migrate Flipkart lite’s product page to AMP pages so that they load up instantly.</li> <li>Made changes in the current progressive web app of Flipkart to react to different actions performed on AMP page.</li> <li>Created a node module called “fk-amp” which abstracts all the files and middlewares and can be easily imported and used from express server.</li>",
+    title: "Technical Development Lead",
+    cardImage: "assets/images/experience-page/tcs.png",
+    place: "Tata Consultancy Services",
+    time: "(2021 - present)",
+    desp: "Working on education solutions as a developer, lead and mentor.",
   },
   {
-    title: "Student Developer",
-    cardImage: "assets/images/experience-page/gsoc.png",
-    place: "Google Summer Of Code",
-    time: "(Mar - Aug, 2019)",
-    desp: "<li>Worked with MOZILLA as a part of Google Summer Of Code.</li><li>Worked on Extension Activity Monitor which is a privileged extension for Firefox that uses the activityLog API (privileged API) to monitor the activities of the other installed extensions.</li><li>The activityLog API listens for logs from other installed extensions.</li>",
+    title: "Modernization Developer",
+    cardImage: "assets/images/experience-page/fresche.png",
+    place: "Fresche Solutions",
+    time: "(2019 - 2021)",
+    desp: "Worked on modernization of legacy applications.",
   },
   {
-    title: "Research Intern",
-    cardImage: "assets/images/experience-page/IIT_Bombay.jpg",
-    place: "IIT, Bombay",
-    time: "(Dec, 19 - Jan, 20)",
-    desp: "<li>Worked on the project “LTI (Learning Tools Interoperability) 2.0 standards Implementation for ekShiksha.</li><li>Designed a software that would help faculty to create a quiz using the questions from the database based on his/her choice of topics and he should be provided with various facilities and options to create a quiz of his choice.</li>",
+    title: "Agile Microservices Developer",
+    cardImage: "assets/images/experience-page/capg.png",
+    place: "Capgemini",
+    time: "(2018 - 2019)",	
+    desp: "Worked on developing microservices for a financial institution.",
   },
+  {
+    title: "Programmer Analyst",
+    cardImage: "assets/images/experience-page/cts.png",
+    place: "Cognizant",
+    time: "(2016 - 2018)",
+    desp: "Worked on application developemnt and cloud configuration for a banking client.",
+  },{
+    title: "Intern",
+    cardImage: "assets/images/experience-page/cris.png",
+    place: "CRIS ( Ministry of Indian Railways )",
+    time: "(2014)",	
+    desp: "Developed a web application for file tracking.",
+  },{
+    title: "Trainee",
+    cardImage: "assets/images/experience-page/cdac.png",
+    place: "CDAC",
+    time: "(2013)",	
+    desp: "Participated in Network Security and Pentesting training program.",
+  }
 ];
 
 const showCards2 = () => {
